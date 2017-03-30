ea94d81cdbb4f954e94899c0515baa4915462b07
Bryden Fogelman <j6b9@lulu.ugrad.cs.ubc.ca>
2017-03-29 21:24:49 -0700

    some work and shit

---
+++ projects/project3/display.sh
@@ -0,0 +1,15 @@
+echo "f 1"
+cat mys01
+echo "f 2"
+cat mys02
+echo "f 3"
+cat mys03
+echo "f 4"
+cat mys04
+echo "f 5"
+cat mys05
+echo "f 6"
+cat mys06
+echo "f 7"
+cat mys07
+
--- projects/project3/generator2.cc
+++ projects/project3/generator2.cc
@@ -11,9 +11,9 @@ int main(int argc, char* argv[])
 
 	int n = atoi(argv[1]);
 
-	for(int i = 1; i <= n; i++) {
-		cout << "I " << 2*i << endl;
-		//cout << "I " << 1 << endl;
+	for(int i = 1; i <= n/2; i++) {
+		cout << "I " << i << endl;
+		cout << "F " << 1 << endl;
 		//cout << "F " << i << endl;
 		//cout << "F " << i << endl;
 	}
